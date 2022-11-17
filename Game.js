ef0637f8d416f0fdf51c5a25f57cc02af09ffa98
123mjb <micahbutt@outlook.com>
2022-11-17 08:11:24 +0000

    Pls

--- Game.js
+++ Game.js
@@ -1,12 +1,15 @@
+colors = ["#2CFF00","#00EDFF","#001CFF","#ff0004","#ff00f5","#ffad00","#000000","#ffffff"]
+
 function startGame() {
     myGameArea.initialize();
     myGameComponent = new component(30, 20, "blue", 20, 120);  
 }
 var myGameArea = {
-    canvas : document.getElementById("Game"),
+    canvas : document.createElement("canvas"),
     initialize : function() {
-        this.context = this.canvas.getContext("2d");
-    this.interval = setInterval(updateGameArea, 20);
+        this.context = this.canvas.getContext('2d');
+        this.interval = setInterval(updateGameArea, 20);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     },
     clear : function() {  
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);  
